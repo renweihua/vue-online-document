e4ca03e2be6bc21bb55354d6b34823b636d82fda
小丑路人 <2278757482@qq.com>
2023-04-18 19:33:15 +0800

    同步项目

---
+++ src/renderer/pages/test/test.vue
@@ -0,0 +1,35 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2021-06-08 19:36
+    模块名称：test页面
+    备注：
+*/
+<template>
+    <div class="wrap">
+        <s-json-editor v-model="data"></s-json-editor>
+        <pre class="w-200px">{{ data }}</pre>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    data() {
+        return {
+            data: ""
+        };
+    },
+    methods: {
+    },
+})
+</script>
+
+<style lang="scss">
+.wrap {
+    display: flex;
+    width: 80vw;
+    height: calc(100vh - 100px);
+    border: 1px solid $gray-400;
+}
+</style>
