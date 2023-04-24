30bab97c069bcd47529bba0042a6ed25eb1228ce
小丑路人 <2278757482@qq.com>
2023-04-24 19:33:46 +0800

    更新文档

--- src/components/doc/docEdit.vue
+++ src/components/doc/docEdit.vue
@@ -1,173 +1,128 @@
 <template>
-  <div class="doc-edit" v-loading="loading">
-    <div class="btn">
-      <el-button plain @click="$router.go(-1)">返 回</el-button>
-      <el-button type="success" plain @click="updateDoc()">保 存</el-button>
-    </div>
-    <div class="doc-wrapper">
-      <DocInfo
-        :doc="doc"
-        :isUpdate="true"
-        :groupList="this.groupList"
-        v-on:update-info="updateInfo($event)"
-      />
-      <div class="doc-content">
-        <mavon-editor v-model="doc.content" ref="md" />
-      </div>
-    </div>
-  </div>
+	<div class="doc-edit" v-loading="loading">
+		<div class="btn">
+			<el-button plain @click="$router.go(-1)">返 回</el-button>
+			<el-button type="success" plain @click="updateDoc()">保 存</el-button>
+		</div>
+		<div class="doc-wrapper">
+			<DocInfo :doc="doc" :isUpdate="true" :groupList="this.groupList" v-on:update-info="updateInfo($event)" />
+			<div class="doc-content">
+				<mavon-editor v-model="doc.content_markdown" ref="md" />
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-import DocInfo from "./units/docInfo";
-const CODE_OK = 200;
+	import DocInfo from "./units/docInfo";
+	const CODE_OK = 200;
+	import {
+		lists as groups
+	} from "@/api/group";
+    import { detail, update } from "@/api/doc"
 
-export default {
-  components: {
-    DocInfo,
-  },
-  name: "docEdit",
-  props: {
-    docId: [Number, String],
-  },
-  data() {
-    return {
-      doc: {},
-      groupList: [],
-      isCreate: true,
-      loading: true,
-    };
-  },
-  methods: {
-    updateInfo(val) {
-      this.doc.title = val.title;
-      this.doc.group_id = val.groupId;
-      this.doc.group_id_second = val.groupIdSecond;
-    },
-    //获取分组列表
-    getGroup(pageSize = 10, curr = 1, projectId) {
-      this.$http
-        .get("/group/list", {
-          params: {
-            cp: curr,
-            type: 3,
-            ps: pageSize,
-            projectId: projectId ? projectId : 0,
-          },
-        })
-        .then(
-          (response) => {
-            response = response.data;
-            if (response.code === CODE_OK) {
-              if (response.data) {
-                this.groupList = response.data;
-              }
-            } else {
-              this.$message.error(
-                "获取数据-操作失败!" + !response.msg ? response.msg : ""
-              );
-            }
-          },
-          () => {
-            this.$message.error("获取数据-操作失败!");
-          }
-        );
-    },
-    //更新文档
-    updateDoc() {
-      if (!this.doc.group_id) {
-        this.$message.error("请选择分组");
-        return;
-      }
+	export default {
+		components: {
+			DocInfo,
+		},
+		name: "docEdit",
+		props: {
+			docId: [Number, String],
+		},
+		data() {
+			return {
+				doc: {},
+				groupList: [],
+				isCreate: true,
+				loading: true,
+			};
+		},
+		methods: {
+			updateInfo(val) {
+				this.doc.doc_name = val.doc_name;
+				this.doc.group_id = val.groupId;
+			},
+			//获取分组列表
+			async getGroup(pageSize = 10, curr = 1, projectId) {
+				const {
+					data,
+					http_status,
+					msg
+				} = await groups({
+					cp: curr,
+					ps: pageSize,
+					project_id: projectId ? projectId : 0,
+					group_type: 1, // 文档分组
+				});
+				this.groupList = data;
+			},
+			//更新文档
+			updateDoc() {
+				if (this.doc.doc_name.length < 1) {
+					this.$message.error('文档标题不可为空！');
+                    return;
+				}
 
-      if (this.doc.title.length < 1) {
-        this.$message.error("标题不能为空");
-      }
+				this.$confirm("确定保存文档`" + this.doc.doc_name + "`吗？", "提示", {
+						confirmButtonText: "确定",
+						cancelButtonText: "取消",
+						type: "warning",
+					})
+					.then(() => {
+						update(this.doc)
+							.then((res) => {
+                                if (res.http_status === CODE_OK) {
+                                    this.$message.success(res.msg);
 
-      this.$confirm("要保存吗？", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          this.$http
-            .post("/doc/update", {
-              title: this.doc.title,
-              content: this.doc.content,
-              group_id: this.doc.group_id_second
-                ? this.doc.group_id_second
-                : this.doc.group_id,
-              id: this.docId,
-            })
-            .then((res) => {
-              res = res.data;
-              if (res.code === CODE_OK) {
-                this.$message.success("更新成功!");
-                this.$router.go(-1);
-              } else {
-                this.$message.error(res.msg);
-              }
-            });
-        })
-        .catch(() => {});
-    },
-    //获取文档详情
-    getDocDetail() {
-      this.$http
-        .get("/doc/detail", {
-          params: {
-            id: this.docId,
-          },
-        })
-        .then(
-          (res) => {
-            let response = res.data;
-            if (response.code === CODE_OK) {
-              this.doc = response.data;
-            } else {
-              this.$message.error(`获取数据失败:${response.msg}`);
-            }
-          },
-          () => {
-            this.$message.error("获取数据失败");
-          }
-        );
-    },
-  },
-  created() {
-    if (!this.docId) {
-      return;
-    }
+                                    this.$router.go(-1);
+                                }
+							});
+					})
+					.catch(() => {});
+			},
+			//获取文档详情
+			async getDocDetail() {
+				const {data, http_status, msg} = await detail(this.docId);
+                if (http_status === CODE_OK) {
+                    this.doc = data;
+                }
+                this.loading = false;
+			},
+		},
+		created() {
+			if (!this.docId) {
+				return;
+			}
 
-    this.getGroup(100, 1, this.$route.params.projectId);
-    this.getDocDetail();
-  },
-  watch: {
-    doc: function (val) {
-      if (val.id) {
-        this.loading = false;
-      }
-    },
-  },
-};
+			this.getGroup(100, 1, this.$route.params.projectId);
+			this.getDocDetail();
+		},
+		watch: {
+			doc: function(val) {
+				if (val.id) {
+					this.loading = false;
+				}
+			},
+		},
+	};
 </script>
 <style lang="scss" scoped>
-.doc-edit {
-  margin: 10px 0 0 0;
+	.doc-edit {
+		margin: 10px 0 0 0;
 
-  .doc-wrapper {
-    margin: 20px auto;
-    min-height: 800px;
-  }
+		.doc-wrapper {
+			margin: 20px auto;
+			min-height: 800px;
+		}
 
-  .doc-content .v-note-wrapper {
-    margin: 10px;
-    height: 1000px;
-  }
+		.doc-content .v-note-wrapper {
+			margin: 10px;
+			height: 1000px;
+		}
 
-  .btn {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-  }
-}
+		.btn {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+		}
+	}
 </style>
