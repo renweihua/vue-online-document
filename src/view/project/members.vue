8db258ba3eb265fde06699f5b386122f26ffb20c
小丑路人 <2278757482@qq.com>
2023-04-25 20:12:45 +0800

    项目成员列表

--- src/view/project/members.vue
+++ src/view/project/members.vue
@@ -16,28 +16,25 @@
 		<!-- 项目用户 -->
 		<div class="all-user" v-loading="loading">
 			<el-table :data="userList" stripe style="width: 100%" border>
-				<el-table-column prop="name" label="账号" width="180"></el-table-column>
-				<el-table-column prop="state" label="状态" width="180">
-					<template slot-scope="scope">{{ transferState(scope.row.state) }}</template>
-				</el-table-column>
+				<el-table-column prop="user_info.nick_name" label="昵称"></el-table-column>
+				<el-table-column prop="user.user_name" label="账号" width="180"></el-table-column>
+				<el-table-column prop="user.user_email" label="邮箱"></el-table-column>
 				<el-table-column prop="is_leader" label="用户类型">
 					<template slot-scope="scope">{{scope.row.is_leader == 1 ? '项目管理者' : '项目成员' }}</template>
 				</el-table-column>
-				<el-table-column prop="nick_name" label="昵称"></el-table-column>
-				<el-table-column prop="email" label="邮箱"></el-table-column>
-				<el-table-column prop="email" label="权限">
-					<template slot-scope="scope">{{scope.row.permission == 6 ? '读/写':'只读'}}</template>
+				<el-table-column prop="role_power" label="权限">
+					<template slot-scope="scope">{{scope.row.role_power == 0 ? '只读' : (scope.row.role_power == 1 ? '读/写' : '创建人')}}</template>
 				</el-table-column>
 				<el-table-column align="center" label="操作">
 					<template slot-scope="scope">
 						<el-dropdown placement="bottom" trigger="click" @command="handleCommand">
-							<el-button type="text">权限设置</el-button>
+							<el-button type="primary">权限设置</el-button>
 							<el-dropdown-menu slot="dropdown">
 								<el-dropdown-item :command="{action:'setPermission',data:scope.row}">
-									设置{{scope.row.permission == 4 ? '读/写':'只读'}}</el-dropdown-item>
+									设置{{scope.row.role_power == 1 ? '只读' : '读/写'}}</el-dropdown-item>
 							</el-dropdown-menu>
 						</el-dropdown>
-						<el-button type="text" style="margin-left:10px" @click="quitProject(scope.row)">移出项目</el-button>
+						<el-button type="danger" style="margin-left:10px" @click="quitProject(scope.row)">移出项目</el-button>
 					</template>
 				</el-table-column>
 			</el-table>
@@ -96,27 +93,11 @@
 				}
 			},
 			//获取项目用户列表
-			getProjectUserList() {
-				this.$http
-					.get("/project/project-user", {
-						params: {
-							id: this.$route.params.projectId,
-						},
-					})
-					.then(
-						(response) => {
-							response = response.data;
-							if (response.code === CODE_OK) {
-								this.userList = response.data;
-							}
-						},
-						(res) => {
-							let response = res.data;
-							this.$message.error(
-								"获取数据-操作失败!" + !response.msg ? response.msg : ""
-							);
-						}
-					);
+			async getProjectUserList() {
+				const {data, http_status, msg} = await lists({
+						project_id: this.$route.params.projectId
+					});
+				this.userList = data.data;
 			},
 			//增加项目用户
 			addProjectUser(val) {
