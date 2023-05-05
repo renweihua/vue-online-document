e57004201fa15ddf2ca1a675a93b35e85a50d640
小丑路人 <2278757482@qq.com>
2023-05-05 16:18:31 +0800

    optimize

--- src/components/api/units/apiInfo.vue
+++ src/components/api/units/apiInfo.vue
@@ -2,64 +2,49 @@
 	<div class="api-info-box">
 		<div class="info">
 			<el-form :model="apiInfo" :rules="rules" ref="form">
-				<el-form-item>
-					<el-col :span="5">
-						<el-form-item prop="group_id" label="分组" label-width="80px">
-							<el-cascader
-							    v-model="apiInfo.group_id"
-							    placeholder="默认为一级分组"
-							    :options="groupList"
-							    :props="{
-									children: '_child',
-									label: 'group_name',
-									expandTrigger: 'hover',
-									// 选择任意一级选项
-									checkStrictly: 'true',
-									// 指定选项的值为选项对象的某个属性值
-									value: 'group_id',
-									// 指定选项标签为选项对象的某个属性值
-									label: 'group_name',
-								}"
-							    clearable
-							    filterable
-					    		@change="selectGroupHandleChange">
-							</el-cascader>
-						</el-form-item>
-					</el-col>
-					<el-col :span="5">
-						<el-form-item label="请求协议" prop="http_protocol" label-width="80px">
-							<el-select v-model="apiInfo.http_protocol" placeholder="选择请求协议">
-								<el-option v-if="propertyList && propertyList.http_protocol" v-for="item in propertyList.http_protocol" :key="item" :label="item" :value="item"></el-option>
-							</el-select>
-						</el-form-item>
-					</el-col>
-
-					<el-col :span="5">
-						<el-form-item label="请求方式" prop="http_method" label-width="80px">
-							<el-select v-model="apiInfo.http_method" placeholder="选择请求方式">
-								<el-option-group
-									v-if="propertyList && propertyList.http_method"
-					                v-for="(group, index) in propertyList.http_method"
-					                :key="index"
-					                :label="group.label"
-					            >
-	                            	<el-option v-for="item in group.options" :key="item" :label="item" :value="item"></el-option>
-	                            </el-option-group>
-							</el-select>
-						</el-form-item>
-					</el-col>
+				<el-form-item prop="group_id" label="分组" label-width="80px">
+					<el-cascader
+					    v-model="apiInfo.group_id"
+					    placeholder="默认为一级分组"
+					    :options="groupList"
+					    :props="{
+							children: '_child',
+							label: 'group_name',
+							expandTrigger: 'hover',
+							// 选择任意一级选项
+							checkStrictly: 'true',
+							// 指定选项的值为选项对象的某个属性值
+							value: 'group_id',
+							// 指定选项标签为选项对象的某个属性值
+							label: 'group_name',
+						}"
+					    clearable
+					    filterable
+			    		@change="selectGroupHandleChange">
+					</el-cascader>
+				</el-form-item>
+				<el-form-item label="请求协议" prop="http_protocol" label-width="80px">
+					<el-select v-model="apiInfo.http_protocol" placeholder="选择请求协议">
+						<el-option v-if="propertyList && propertyList.http_protocol" v-for="item in propertyList.http_protocol" :key="item" :label="item" :value="item"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="请求方式" prop="http_method" label-width="80px">
+					<el-select v-model="apiInfo.http_method" placeholder="选择请求方式">
+						<el-option-group
+							v-if="propertyList && propertyList.http_method"
+			                v-for="(group, index) in propertyList.http_method"
+			                :key="index"
+			                :label="group.label"
+			            >
+                        	<el-option v-for="item in group.options" :key="item" :label="item" :value="item"></el-option>
+                        </el-option-group>
+					</el-select>
 				</el-form-item>
 				<el-form-item label="api_url" prop="api_url" label-width="80px">
 					<el-input v-model="apiInfo.api_url"></el-input>
 				</el-form-item>
 				<el-form-item label="名称" prop="api_name" label-width="80px">
 					<el-input v-model="apiInfo.api_name"></el-input>
-				</el-form-item>
-				<el-form-item label="对象名" prop="object_name" label-width="80px">
-					<el-input v-model="apiInfo.object_name" placeholder="非必填"></el-input>
-				</el-form-item>
-				<el-form-item label="方法名" prop="function_name" label-width="80px">
-					<el-input v-model="apiInfo.function_name" placeholder="非必填"></el-input>
 				</el-form-item>
 			</el-form>
 		</div>
@@ -123,8 +108,6 @@
 					http_return_type: "", //返回值类型
 					url: "", //http请求URL
 					api_name: "", //接口名称
-					object_name: "", // 对象名
-					function_name: "", // 程序内部方法名
 					develop_language: "", // 接口开发语言
 					id: 0,
 				},
@@ -146,7 +129,7 @@
 					for (let item in val) {
 						switch (item) {
 							case "http_method":
-								this.apiInfo.http_method = val[item][0];
+								this.apiInfo.http_method = val[item][0].options[0];
 								break;
 							case "http_protocol":
 								this.apiInfo.http_protocol = val[item][0];
@@ -163,6 +146,9 @@
 </script>
 
 <style lang="scss" scoped>
+	.el-cascader, .el-select{
+		width: 100%;
+	}
 	dl dd div {
 		display: inline-block;
 		width: 95%;
